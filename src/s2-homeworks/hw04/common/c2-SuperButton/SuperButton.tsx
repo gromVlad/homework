8f8d96ccffae8436dd0c33f184ffcb2570221f0d
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2023-04-13 18:34:00 +0300

    4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,10 +17,12 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName =
-      s.button +
-      + (disabled ? " " + s.disabled : xType === 'red' ? s.red : " ") +
-      (className ? " " + className : ""); // задачка на смешивание классов
+    const finalClassName = s.button
+        + (disabled
+                ? " " + s.disabled 
+                : xType === 'red'
+                    ? " " + s.red : " " + s.secondary)
+        + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
         <button
