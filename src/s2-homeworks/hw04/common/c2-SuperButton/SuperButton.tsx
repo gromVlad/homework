d8e0cf25810048abf2fe1dc74236a005b3fdeb31
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2023-04-13 18:42:22 +0300

    hello

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -21,7 +21,8 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         + (disabled
                 ? " " + s.disabled 
                 : xType === 'red'
-                    ? " " + s.red : " " + s.secondary)
+                    ? " " + s.red 
+                    : xType === 'secondary' ? " " + s.secondary : ' ' + s.default)
         + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
