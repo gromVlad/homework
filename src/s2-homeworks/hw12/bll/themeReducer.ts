5958c64fe8b8039fab624be3ba228bb531b5a166
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2023-06-08 09:32:00 +0300

    12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,20 @@
 const initState = {
     themeId: 1,
 }
+type InitStateType = typeof initState
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+export const themeReducer = (
+  state = initState,
+  action: ChangeThemeIdType
+): InitStateType => {
+  // fix any
+  switch (action.type) {
+    case "SET_THEME_ID":
+      return { ...state, themeId: action.id };
+    default:
+      return state;
+  }
+};
 
-        default:
-            return state
-    }
-}
-
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id } as const) // fix any
+type ChangeThemeIdType = ReturnType<typeof changeThemeId>
