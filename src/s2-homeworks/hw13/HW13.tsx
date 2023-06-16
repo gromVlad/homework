65a0d155c424f213a4330e8c5cca6c53c1b30bf3
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2023-06-16 15:35:42 +0300

    les 13

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -9,94 +9,99 @@ import error500 from "./images/500.svg";
 import errorUnknown from "./images/error.svg";
 
 /*
+ * 1 - дописать функцию send
+ * 2 - дизэйблить кнопки пока идёт запрос
+ * 3 - сделать стили в соответствии с дизайном
+ * */
 
-1 - дописать функцию send
-2 - дизэйблить кнопки пока идёт запрос
-3 - сделать стили в соответствии с дизайном
-*/
 const HW13 = () => {
   const [code, setCode] = useState("");
   const [text, setText] = useState("");
   const [info, setInfo] = useState("");
   const [image, setImage] = useState("");
-  const [isload, setIsload] = useState(false);
 
   const send = (x?: boolean | null) => () => {
     const url =
       x === null
-        ? "https://xxxxxx.ccc"
+        ? "https://xxxxxx.ccc" // имитация запроса на не корректный адрес
         : "https://incubator-personal-page-back.herokuapp.com/api/3.0/homework/test";
+
     setCode("");
     setImage("");
     setText("");
     setInfo("...loading");
-    setIsload(true);
+
     axios
       .post(url, { success: x })
       .then((res) => {
         setCode("Код 200!");
+        setText(res.data.errorText);
         setImage(success200);
-        setText("");
+        // дописать
         setInfo("");
-        setIsload(false);
       })
       .catch((e) => {
-        if (e.response.status === 400) {
-          setCode("Код ошибки 400");
-          setText("Ошибка на стороне клиента");
-          setImage(error400);
-          setInfo("Неправильный запрос");
-          setIsload(false);
-        } else if (e.response.status === 500) {
-          setCode("Код ошибки 500");
-          setText("Ошибка на стороне сервера");
+        // дописать
+        if (e.code === "ERR_BAD_RESPONSE") {
+          setCode("Error 500!");
+          setText(e.response.data.errorText);
           setImage(error500);
-          setInfo("Ошибка сервера");
-          setIsload(false);
-        } else {
-          setCode("Неизвестная ошибка");
-          setText("Что-то пошло не так...");
+          setInfo("");
+        }
+        if (e.code === "ERR_BAD_REQUEST") {
+          setCode("Error 400!");
+          setText(e.response.data.errorText);
+          setImage(error400);
+          setInfo("");
+        }
+        if (e.code === "ERR_NETWORK") {
+          setCode("Error Unknown!");
+          setText(e.response.data.errorText);
           setImage(errorUnknown);
-          setInfo("errorUnknown");
-          setIsload(false);
+          setInfo("");
         }
       });
   };
 
   return (
-    <div id={"hw13"} >
+    <div id={"hw13"}>
       <div className={s2.hwTitle}>Homework #13</div>
+
       <div className={s2.hw}>
         <div className={s.buttonsContainer}>
           <SuperButton
             id={"hw13-send-true"}
             onClick={send(true)}
-            disabled={isload}
             xType={"secondary"}
+            // дописать
+            disabled={info !== ""}
           >
             Send true
           </SuperButton>
           <SuperButton
             id={"hw13-send-false"}
             onClick={send(false)}
-            disabled={isload}
             xType={"secondary"}
+            // дописать
+            disabled={info !== ""}
           >
             Send false
           </SuperButton>
           <SuperButton
             id={"hw13-send-undefined"}
             onClick={send(undefined)}
-            disabled={isload}
             xType={"secondary"}
+            // дописать
+            disabled={info !== ""}
           >
             Send undefined
           </SuperButton>
           <SuperButton
             id={"hw13-send-null"}
-            onClick={send(null)}
-            disabled={isload}
+            onClick={send(null)} // имитация запроса на не корректный адрес
             xType={"secondary"}
+            // дописать
+            disabled={info !== ""}
           >
             Send null
           </SuperButton>
