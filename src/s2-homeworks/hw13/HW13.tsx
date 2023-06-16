5b03b33b44441d0e2eafbb128ea2b9e6243e28a5
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2023-06-16 17:51:50 +0300

    DZ13

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -19,7 +19,6 @@ const HW13 = () => {
   const [text, setText] = useState("");
   const [info, setInfo] = useState("");
   const [image, setImage] = useState("");
-  const [bool, setbool] = useState(false);
 
   const send = (x?: boolean | null) => () => {
     const url =
@@ -27,112 +26,107 @@ const HW13 = () => {
         ? "https://xxxxxx.ccc" // имитация запроса на не корректный адрес
         : "https://samurai.it-incubator.io/api/3.0/homework/test";
 
-    setCode("Error!");
-    setImage(errorUnknown);
-    setText("Network Error");
-    setInfo("AxiosError");
-    setbool(true)
+    setCode("");
+    setImage("");
+    setText("");
+    setInfo("...loading");
+
     axios
-      .post(url, { success: x })
+      .post(url, { success: x },)
       .then((res) => {
-        setCode("Код 200!");
+        console.log(res.status);
+        
+        setCode(`Код ${res.status}!`);
         setImage(success200);
         // дописать
-        setText("...всё ок)");
-        setInfo("код 200 - обычно означает что скорее всего всё ок)");
+        setInfo(res.data.info);
+        setText(res.data.errorText);
       })
       .catch((e) => {
-        if (e.response === undefined) {
-          setCode("Error!");
+        // дописать
+        if (e.response.status) {
+          console.log(e.response.data.errorText);
+          setCode(`Ошибка ${e.response.status}!!`);
+          setImage(e.response.status === 500 ? error500 : error400);
+          setInfo(e.response.data.info);
+          setText(e.response.data.errorText);
+        } else {
+          // console.log(e.name, e.message)
           setImage(errorUnknown);
-          setText("Network Error");
-          setInfo("AxiosError");
+          setCode("Error");
+          setInfo(e.name);
+          setText(e.message);
         }
-        if (e.response)
-          switch (e.response.status) {
-            case 400:
-              setCode("Ошибка 400!");
-              setImage(error400);
-              setText("Ты не отправил success в body вообще!");
-              setInfo(
-                "ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!"
-              );
-              break;
-            case 500:
-              setCode("Ошибка 500!");
-              setImage(error500);
-              setText("эмитация ошибки на сервере");
-              setInfo(
-                "ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)"
-              );
-              break;
-          }
-      })
-      .finally(() => setbool(false))
+      });
   };
 
   return (
     <div id={"hw13"}>
-      <div className={s2.hwTitle}>Homework #13</div>
-
-      <div className={s2.hw}>
-        <div className={s.buttonsContainer}>
-          <SuperButton
-            id={"hw13-send-true"}
-            onClick={send(true)}
-            xType={"secondary"}
-            // дописать
-            disabled={bool}
-          >
-            Send true
-          </SuperButton>
-          <SuperButton
-            id={"hw13-send-false"}
-            onClick={send(false)}
-            xType={"secondary"}
-            // дописать
-            disabled={bool}
-          >
-            Send false
-          </SuperButton>
-          <SuperButton
-            id={"hw13-send-undefined"}
-            onClick={send(undefined)}
-            xType={"secondary"}
-            // дописать
-            disabled={bool}
-          >
-            Send undefined
-          </SuperButton>
-          <SuperButton
-            id={"hw13-send-null"}
-            onClick={send(null)} // имитация запроса на не корректный адрес
-            xType={"secondary"}
-            // дописать
-            disabled={bool}
-          >
-            Send null
-          </SuperButton>
-        </div>
-
-        <div className={s.responseContainer}>
-          <div className={s.imageContainer}>
-            {image && <img src={image} className={s.image} alt="status" />}
+      <div className={s2.container}>
+        <div className={s2.hwTitle}>Homework №13</div>
+      </div>
+      <hr />
+      <div className={s2.container}>
+        <div className={s2.hw}>
+          <div className={s.buttonsContainer}>
+            <SuperButton
+              id={"hw13-send-true"}
+              onClick={send(true)}
+              xType={"secondary"}
+              // дописать
+              disabled={info === "...loading"}
+            >
+              Send true
+            </SuperButton>
+            <SuperButton
+              id={"hw13-send-false"}
+              onClick={send(false)}
+              xType={"secondary"}
+              // дописать
+              disabled={info === "...loading"}
+            >
+              Send false
+            </SuperButton>
+            <SuperButton
+              id={"hw13-send-undefined"}
+              onClick={send(undefined)}
+              xType={"secondary"}
+              // дописать
+              disabled={info === "...loading"}
+            >
+              Send undefined
+            </SuperButton>
+            <SuperButton
+              id={"hw13-send-null"}
+              onClick={send(null)} // имитация запроса на не корректный адрес
+              xType={"secondary"}
+              // дописать
+              disabled={info === "...loading"}
+            >
+              Send null
+            </SuperButton>
           </div>
 
-          <div className={s.textContainer}>
-            <div id={"hw13-code"} className={s.code}>
-              {code}
+          <div className={s.responseContainer}>
+            <div className={s.imageContainer}>
+              {image && <img src={image} className={s.image} alt="status" />}
             </div>
-            <div id={"hw13-text"} className={s.text}>
-              {text}
-            </div>
-            <div id={"hw13-info"} className={s.info}>
-              {info}
+
+            <div className={s.textContainer}>
+              <div id={"hw13-code"} className={s.code}>
+                {code}
+              </div>
+              <div id={"hw13-text"} className={s.text}>
+                {text}
+              </div>
+              <div id={"hw13-info"} className={s.info}>
+                {info}
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <hr />
     </div>
   );
 };
